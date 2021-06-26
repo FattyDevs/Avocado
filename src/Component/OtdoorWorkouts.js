336ca321874beb60f0b28abad26897c0d9a06c67
RoaaMustafa <labushanab14@gmail.com>
2021-06-26 19:52:43 +0300

    create outdoorWorkouts

---
+++ src/Component/OtdoorWorkouts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class OutdoorWorkouts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+  render() {
+    return <>here will shown cards</>;
+  }
+}
+export default OutdoorWorkouts;
