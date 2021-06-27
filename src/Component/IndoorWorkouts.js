4bbb8f386788883ae343fd48f774358ba30c7390
Nadeemmazahreh <nadeemmazahreh97@hotmail.com>
2021-06-27 16:42:51 +0300

    Indoor page done + Sports page css done

--- src/Component/IndoorWorkouts.js
+++ src/Component/IndoorWorkouts.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, CardColumns } from "react-bootstrap";
 
 
 class IndoorWorkouts extends React.Component{
 render(){
     return (
         <>
-        here will shown cards
+         <CardColumns>
+          {this.props.displayIn.map((item, indx) => {
+              return (
+                <div>
+                <Card className="text-center" key={indx}>
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Img src={item.image} />
+                  </Card.Body>
+                  <Card.Footer className="text-muted">{item.description}</Card.Footer>
+                </Card>
+              </div>
+            )
+          }
+          )}
+        </CardColumns>
         </>
     )
 }
