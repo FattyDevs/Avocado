28f5a046a11b323877b3614f9a540399da6383dd
shaheenaz <shaheenaz99@gmail.com>
2021-06-27 17:26:04 +0300

    add backend

--- src/Component/ResultFood.js
+++ src/Component/ResultFood.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Carousel, Container, Row, Col, CardColumns, Card, Button, Form, Modal } from 'react-bootstrap';
 
 class ResultFood extends React.Component{
-    state={
-        showIngredient:false,
-        caloriesNum:0,
-       
+    constructor(props){
+        super(props);
+        this.state={
+            showIngredient:false,
+            caloriesNum:0,
+            favDish :0
+           
+        }
     }
+    
     
     componentWillMount(){
         let calories=parseInt(this.props.calories)/parseInt(this.props.yield)
@@ -16,8 +21,29 @@ class ResultFood extends React.Component{
         console.log(calories)
     }
         
-     
+    favDishs=()=>{
+        const AddFood = {
+            label:label,
+            image:image,
+            url:url,
+            yield:yield,
+            calories:calories,
+            mealType:mealType,
+        }
+        this.setState({
+            
+            favDish : this.state.favDish+1
+        
+        })
+  console.log("this.state.favDish")
+    
 
+    }
+
+
+
+
+ 
     // showIngredientFunc=()=>{
     //     this.setState({
     //         showIngredient:true,
@@ -40,7 +66,7 @@ Contact=()=> {
         return(
             <>
     <Col>
-    <Card style={{ width: '25rem' }} >
+    <Card   style={{ width: '25rem' }} >
   <Card.Img variant="top" src={this.props.image} onClick={this.Contact}/>
   <Card.Body>
     <Card.Title>{this.props.label}</Card.Title>
@@ -49,6 +75,7 @@ Contact=()=> {
 <Col><Card.Text>{this.state.caloriesNum}Cal/Serving</Card.Text></Col>
 <Col><Card.Text>Serving:{this.props.yield}</Card.Text></Col>
 <Col>{this.props.mealType}</Col>
+<Col onClick={this.favDishs}> 💟 {this.state.favDish}</Col>
           </Row>
  
    
