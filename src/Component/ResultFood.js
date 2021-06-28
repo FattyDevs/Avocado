4d5fe4c9946b0c730c9a64e7a3a3c828a8a07a48
shaheenaz <shaheenaz99@gmail.com>
2021-06-28 17:57:27 +0300

    add styling and colors

--- src/Component/ResultFood.js
+++ src/Component/ResultFood.js
@@ -136,19 +136,32 @@ Contact=()=> {
   </Row>
 </Container>
 </CardColumns> */}
-                {/* <Button onClick={this.getting} variant="outline-secondary" id="button-n2">get</Button> */}
+                {/* <Button onClick={this.getting}  id="button-n2">get</Button> */}
 
     <Col>
-    <Card   style={{ width: '21rem' ,backgroundColor:'#e0e2db',marginTop:"1rem"}} >
-  <Card.Img variant="top" src={this.props.image} onClick={this.Contact}/>
-  <Card.Body>
-    <Card.Title>{this.props.label}</Card.Title>
+    <Card id="searchFoodCard"  >
+     
+  <Card.Img  id="image" variant="top" src={this.props.image} />
+  
+  <Card.Body id="cardBody">
+    <Card.Title class="title">{this.props.label}</Card.Title>
     <Container>
           <Row>
-<Col><Card.Text>{this.state.caloriesNum}Cal/Serving</Card.Text></Col>
-<Col><Card.Text>Serving:{this.props.yield}</Card.Text></Col>
-<Col>{this.props.mealType}</Col>
-<Col onClick={this.favDishs}> 💟 </Col>
+<Col><Card.Text class="calories">{this.state.caloriesNum}Cal/Serving</Card.Text></Col>
+<Col><Card.Text id="yield">Serving:{this.props.yield}</Card.Text></Col>
+<Col  id="mealType">{this.props.mealType}</Col>
+<Col id="favClick">
+ 
+<Button id ="buttonMore" variant="outline-secondary" onClick={this.Contact}>More..</Button>
+{/* <Button id ="buttonFav" variant="outline-secondary" onClick={this.favDishs}>
+    <img onClick={this.favDishs} id ="buttonFav" src="https://img.icons8.com/ios-glyphs/30/fa314a/filled-like.png"/> 
+    </Button> */}
+
+    <img id ="buttonFav" onClick={this.favDishs}  src="https://img.icons8.com/ios-glyphs/40/fa314a/filled-like.png"/>
+    <p id="hovering"> hii 
+    </p>
+</Col>
+{/* <Col id="favClick"onClick={this.favDishs}></Col> */}
           </Row>
  
    
