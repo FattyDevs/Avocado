d83a39bf028481ed6898efa54e0483eb14311392
Al-dasouqi <aldasouqif@gmail.com>
2021-06-30 01:03:35 +0300

    Main page stayle done

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
 import './Home.css'
 //import axios from 'axios';
-import Slider from './Slider.js';
+// import Slider from './Slider.js';
+
+import Slider from '../components/Slider.js';
+// import Test from './Test.js';
+import Info from "../components/InfoSection";
 
 class Home extends React.Component{
 
@@ -12,6 +16,7 @@ class Home extends React.Component{
         return(
             <>
             <Slider />
+            <Info />
             </>
         )
     }
---
+++ src/components/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Exercise',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Nutrition',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Join US',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
